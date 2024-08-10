63b48c306c5ab28c5e011a7a142ec3d85b476309
uday-ss <udhayakumar@softsuave.com>
2024-08-10 23:43:25 +0530

    remove unwanted logs

--- src/hooks/useCustomNotification.ts
+++ src/hooks/useCustomNotification.ts
@@ -2,13 +2,10 @@ import { onMessage } from 'firebase/messaging';
 import { useEffect } from 'react';
 import { messaging } from '../firebase/config';
 
-// if we want to use custom popup for notification, we can use this hook
-
 export function useCustomNotification() {
   useEffect(() => {
-    console.log('called');
     onMessage(messaging, () => {
-      console.log('working fine ');
+      // implement custom popup for notification
     });
   }, []);
 }
