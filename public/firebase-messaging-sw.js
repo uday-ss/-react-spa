a119dd081c229a4e7e429d742c2a48a005cae86e
uday-ss <udhayakumar@softsuave.com>
2024-08-14 22:43:41 +0530

    add firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,8 +4,17 @@ importScripts(
   'https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js'
 );
 
+const config = {
+  apiKey: 'AIzaSyDxZyTT6lSJf869ZGgMzGDSMkHG3YqVWVM',
+  authDomain: 'react-spa-typescript-14467.firebaseapp.com',
+  projectId: 'react-spa-typescript-14467',
+  storageBucket: 'react-spa-typescript-14467.appspot.com',
+  messagingSenderId: '550486223429',
+  appId: '1:550486223429:web:1c109debddd19011f9d12c',
+};
 
-// firebase.initializeApp();
+// replace with your firebase config key
+firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 
