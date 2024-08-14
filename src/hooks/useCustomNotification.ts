34edda50591db3b2c0214a3cc73ec612cbe46e63
uday-ss <udhayakumar@softsuave.com>
2024-08-14 22:40:16 +0530

    show both toast and in built notification

--- src/hooks/useCustomNotification.ts
+++ src/hooks/useCustomNotification.ts
@@ -7,6 +7,15 @@ import { toast } from 'react-toastify';
 export function useCustomNotification() {
   useEffect(() => {
     onMessage(messaging, (payload) => {
+      if ('Notification' in window) {
+        if (Notification.permission === 'granted') {
+          new Notification('Hello!', {
+            body: payload.notification?.body,
+          });
+        } else {
+          console.log('Notification permission is not granted.');
+        }
+      }
       toast(payload.notification?.body);
     });
   }, []);
