c3f6ecbb7b974543214e4709fadccfddffb2ffc1
uday-ss <udhayakumar@softsuave.com>
2024-08-14 23:08:23 +0530

    add config for firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,6 +4,19 @@ importScripts(
   'https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js'
 );
 
+// replace with your firebase config key
+
+firebase.initializeApp({
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+});
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
